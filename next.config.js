8837bc7746921481248b04ec41d862bd571a4ab4
Lychosand <quinn.stevenson@tuta.io>
2023-03-04 08:17:45 -0500

    adding tokens file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
+import Tokens from "./tokens.json";
 
 module.exports = {
 	async redirects() {
-		let yKey = "WCD77E8N";
-		let yClientId = "dj0yJmk9TUdiUTFpU2c2S0g0JmQ9WVdrOVYwTkVOemRGT0U0bWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWNj";
-		let yClientSecret = "72abdce46c9c2e7d79c6bc287426987a11a37b43";
-
 		let yReturnURL = "https://fantasy-hockey-scheduler.vercel.app/posts/api-success";
-		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + yClientId + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
+		
+		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + Tokens['clientId'][0] + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
 
   		return [
 		  	{
