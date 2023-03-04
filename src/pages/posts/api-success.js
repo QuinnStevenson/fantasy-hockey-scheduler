704152d4b8cc98d12b9d1852c679469e8276d968
Lychosand <quinn.stevenson@tuta.io>
2023-03-04 08:43:26 -0500

    Handling access token after authorization success

--- src/pages/posts/api-success.js
+++ src/pages/posts/api-success.js
@@ -1,3 +1,8 @@
+import { useRouter } from 'next/router'
+
 export default function FirstPost() {
+  const router = useRouter();
+  console.log(router.pathname);
+  
   return <h1>Hello Yahoo</h1>;
 }
