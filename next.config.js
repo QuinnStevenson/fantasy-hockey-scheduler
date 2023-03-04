f6e237605b75f59f28d9ea5bde770bacf1d6d43c
Lychosand <quinn.stevenson@tuta.io>
2023-03-04 08:37:53 -0500

    working json tokens

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-import Tokens from "./tokens.json";
+const Tokens = require("./tokens.json");
 
 module.exports = {
 	async redirects() {
 		let yReturnURL = "https://fantasy-hockey-scheduler.vercel.app/posts/api-success";
-		
-		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + Tokens['clientId'][0] + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
+
+		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + Tokens.appId + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
 
   		return [
 		  	{
