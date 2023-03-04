e5ef1850b963f536b69ce9aee46577afa23fba43
Lychosand <quinn.stevenson@tuta.io>
2023-03-04 07:47:44 -0500

    uri not working

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ module.exports = {
 		let yKey = "HQhJM6Lx";
 		let yClientId = "dj0yJmk9NlBlVTN0RDZOcW95JmQ9WVdrOVNGRm9TazAyVEhnbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTlh";
 		let yReturnURL = "https://fantasy-hockey-scheduler.vercel.app/";
-
-		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + yClientId + "&redirect_uri=oob&response_type=code&language=en-us";
+        //                https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9ak5IZ2x5WmNsaHp6JmQ9WVdrOVNqQkJUMnRYTjJrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1hYQ--&redirect_uri=oob&response_type=code&language=en-us
+		//let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9NlBlVTN0RDZOcW95JmQ9WVdrOVNGRm9TazAyVEhnbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTlh--&redirect_uri=oob&response_type=code&language=en-us;
 
   		return [
 		  	{
 		  		source: '/',
-		  		destination: redirect,
+		  		destination: "https://api.login.yahoo.com/oauth2/request_auth?client_id=dj0yJmk9NlBlVTN0RDZOcW95JmQ9WVdrOVNGRm9TazAyVEhnbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTlh--&redirect_uri=oob&response_type=code&language=en-us",
 		  		permanent: false,
 		  	},
 	  	]
