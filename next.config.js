d5ee14473f9202aa41316c874bf85e52b81de25d
Lychosand <quinn.stevenson@tuta.io>
2023-03-04 07:05:08 -0500

    wrong client ID

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@
 module.exports = {
 	async redirects() {
 		let yKey = "HQhJM6Lx";
-		let ySecret = "dj0yJmk9NlBlVTN0RDZOcW95JmQ9WVdrOVNGRm9TazAyVEhnbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTlh";
+		let yClientId = "dj0yJmk9NlBlVTN0RDZOcW95JmQ9WVdrOVNGRm9TazAyVEhnbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTlh";
 		let yReturnURL = "https://fantasy-hockey-scheduler.vercel.app/";
 
-		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + yKey + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
+		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + yClientId + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
 
   		return [
 		  	{
