86a5e61b619623db772919b0ca13ea22cc60c850
Lychosand <quinn.stevenson@tuta.io>
2023-03-04 06:57:37 -0500

    Yahoo oAuth redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+	async redirects() {
+		let yKey = "HQhJM6Lx";
+		let ySecret = "dj0yJmk9NlBlVTN0RDZOcW95JmQ9WVdrOVNGRm9TazAyVEhnbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PTlh";
+		let yReturnURL = "https://fantasy-hockey-scheduler.vercel.app/";
+
+		let redirect = "https://api.login.yahoo.com/oauth2/request_auth?client_id=" + yKey + "&redirect_uri=" + yReturnURL + "&response_type=code&language=en-us";
+
+  		return [
+		  	{
+		  		source: '/',
+		  		destination: redirect,
+		  		permanent: false,
+		  	},
+	  	]
+	},
+
+	reactStrictMode: true,
+}
