eb09bbcd9b6f08eddf354f244f7baddb80f7ba0a
Lychosand <quinn.stevenson@tuta.io>
2023-12-11 09:23:22 -0500

    Db connection

--- src/pages/api/dbPosts.js
+++ src/pages/api/dbPosts.js
@@ -6,14 +6,16 @@ const handler = async(req, res) => {
 
 	switch(req.method) {
 		case "POST":
-			const bodyObject = req.body;
-			delete bodyObject._id;
-			const update = await db.collection("yahooApiData").findOneAndReplace({appId: bodyObject.appId}, bodyObject);
-			res.json({status: 200, data: update});
+			console.log("POSTING")
+			//const bodyObject = req.body;
+			//delete bodyObject._id;
+			//const update = await db.collection("yahooApiData").findOneAndReplace({appId: bodyObject.appId}, bodyObject);
+			//res.json({status: 200, data: update});
 			break;
 		case "GET":
-			const yahooData = await db.collection("yahooApiData").find({}).toArray();
-			res.json({status: 200, data: yahooData});
+			console.log("GETTING")
+			//const yahooData = await db.collection("yahooApiData").find({}).toArray();
+			//res.json({status: 200, data: yahooData});
 			break;
 	}
 }
